ee913ee75bb46985513a5f35c30444c39428ed30
Eugene P <work.petroff@gmail.com>
2026-01-21 16:19:38 +0000

    full refactor cleanup

--- post-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install fzf bat ripgrep build-essential cmake perl pkg-config libclang-dev musl-tools git -y
-sudo ln -s /usr/bin/batcat /usr/bin/bat
-
-# pip install -r ${PWD}/app/requirements.txt
-
-cd ${PWD}/web
-yarn install
